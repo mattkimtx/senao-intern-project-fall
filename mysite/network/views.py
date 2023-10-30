ba90e6d45fdc932da4e5c2b1d1910a0c048d2043
Matthew Kim <matthewkimtx@gmail.com>
2023-10-30 10:57:09 +0800

    working search bar

--- mysite/network/views.py
+++ mysite/network/views.py
@@ -8,15 +8,6 @@ from django.views import generic
 from .models import TextLogs, Company
 from .forms import SearchForm
 
-# class IndexView(generic.ListView):
-#     template_name = "network/templates/mysite/index.html"
-
-#     def get_queryset(self): 
-
-#         query = self.request.GET.get("q")
-#         object_list = TextLogs.objects.filter(eventTitle__icontains=query)
-#         return object_list
-
 class IndexView(generic.ListView):
     model = TextLogs
     template_name = "network/index.html"
@@ -30,22 +21,26 @@ class SearchResultsView(generic.ListView):
 
     def get_queryset(self):
         query = self.request.GET.get('q')
-        return TextLogs.objects.filter(eventTitle__icontains=query).order_by('-category')
+        objects = TextLogs.objects.filter(eventTitle__contains=query).order_by('-category')
+        return objects
 
+def searchEvents(request):
+    if request.method == "POST":
+        searched = request.POST['q']
 
-def findEvent(request):
-    # if this is a post request
-    if request.method == "GET":
-        # create a form with data from request
-        form = SearchForm(request.POST)
-        # check if valid
-        if form.is_valid():
-            # get the data
-            query = form.cleaned_data["q"]
-            search_query = form.get_results(query)
-            # redirect to a new URL:
-            return HttpResponseRedirect("/network/index.html")
-    else:
-        form = SearchForm()
+# def findEvent(request):
+#     # if this is a post request
+#     if request.method == "GET":
+#         # create a form with data from request
+#         form = SearchForm(request.POST)
+#         # check if valid
+#         if form.is_valid():
+#             # get the data
+#             query = form.cleaned_data["q"]
+#             search_query = form.get_results(query)
+#             # redirect to a new URL:
+#             return HttpResponseRedirect("/network/index.html")
+#     else:
+#         form = SearchForm()
 
-    return render(request, "network/index.html", {"form": form})
+#     return render(request, "network/index.html", {"form": form})
